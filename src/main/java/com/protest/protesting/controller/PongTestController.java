8e97fad10aedf9862094ab20f60e776e5ec52f1a
서태완 <taewanseo@astronsec.com>
2022-05-02 15:52:22 +0900

    mmg

--- src/main/java/com/protest/protesting/controller/PongTestController.java
+++ src/main/java/com/protest/protesting/controller/PongTestController.java
@@ -18,7 +18,7 @@ public class PongTestController {
     @Autowired
     private AccountMapper accountMapper;
 
-    @GetMapping("/targetPing/{vvvvvv}")
+    @GetMapping("/targetPing/{tztz}")
     public void pubPing(@PathVariable("seq") String seq) {
         Jedis jedis = new RedisUtil().getInstance();
         jedis.publish("TPingsdfsdfsdfsd", seq);
